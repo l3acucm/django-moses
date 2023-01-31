12818bec6640b9aec6d2db545abf4041df3930f4
Vassily <vassily.v.v@gmail.com>
2023-01-31 21:23:41 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(zip_safe=False, install_requires=[
     'django',
     'djangorestframework-simplejwt',
-    'djoser',
+    'djoser @ git+ssh://git@github.com/sunscrapers/djoser.git',
     'pyotp'
 ], packages=find_packages(exclude=['test_project']))
