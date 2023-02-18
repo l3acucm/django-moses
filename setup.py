10e8f761fae7961b9e78fc42d94a9cdaca2c3317
Vassily <vassily.v.v@gmail.com>
2023-02-18 15:26:36 +0600

    pyproject.toml

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(zip_safe=False, install_requires=[
-    'django',
-    'djangorestframework-simplejwt',
-    'djoser @ https://github.com/sunscrapers/djoser/',
-    'pyotp'
-], packages=find_packages(exclude=['test_project']))
