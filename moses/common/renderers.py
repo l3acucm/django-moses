5d82f5de2be16104e0e45a8afd907ef6bc28a481
l3acucm <l3acucm@gmail.com>
2025-07-27 00:32:18 +0600

    0.12.5

--- moses/common/renderers.py
+++ moses/common/renderers.py
@@ -3,13 +3,17 @@ from rest_framework.renderers import JSONRenderer
 
 class CustomJSONRenderer(JSONRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
+        # If data already has the expected structure, return it as-is
+        if isinstance(data, dict) and "errors" in data and "data" in data:
+            return super().render(data, accepted_media_type, renderer_context)
+
         response_data = {"errors": {}, "data": {}}
 
         if renderer_context:
             response = renderer_context["response"]
             if response.status_code < 300:
                 if response.status_code == 204:
-                    response_data["data"] = []
+                    response_data["data"] = None
                 else:
                     response_data["data"] = data
             else:
