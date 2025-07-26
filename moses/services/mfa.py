f364d28a91d123b9e8455c66759b90b043b59026
l3acucm <l3acucm@gmail.com>
2025-07-26 23:17:36 +0600

    version bump

--- moses/services/mfa.py
+++ moses/services/mfa.py
@@ -1,10 +1,10 @@
 import pyotp as pyotp
 from django.conf import settings as django_settings
 
-def check_mfa_otp(self, otp):
-    if self.is_superuser and not django_settings.DEBUG and not self.mfa_secret_key:
+def check_mfa_otp(user, otp):
+    if user.is_superuser and not django_settings.DEBUG and not user.mfa_secret_key:
         return False
-    elif not self.mfa_secret_key:
+    elif not user.mfa_secret_key:
         return True
-    totp = pyotp.totp.TOTP(self.mfa_secret_key.encode('utf-8'))
+    totp = pyotp.totp.TOTP(user.mfa_secret_key.encode('utf-8'))
     return totp.verify(otp)
