1b4208d493edf8fd0ebf1b4f2e2d6435fb52b363
l3acucm <l3acucm@gmail.com>
2025-07-27 00:03:40 +0600

    one more attempt for renderer

--- moses/common/renderers.py
+++ moses/common/renderers.py
@@ -1,12 +1,20 @@
 from rest_framework.renderers import JSONRenderer
 
+
 class CustomJSONRenderer(JSONRenderer):
     def render(self, data, accepted_media_type=None, renderer_context=None):
         response_data = {"errors": {}, "data": {}}
+
         if renderer_context:
             response = renderer_context["response"]
             if response.status_code < 300:
-                response_data["data"] = data
+                if response.status_code == 204:
+                    response_data["data"] = []
+                else:
+                    response_data["data"] = data
             else:
-                response_data = data
+                response_data["errors"] = data if data else {}
+        else:
+            response_data["data"] = data
+
         return super().render(response_data, accepted_media_type, renderer_context)
