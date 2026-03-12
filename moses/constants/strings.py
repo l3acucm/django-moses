5a23dbe450bdbb2bf2115383208c50ea207b627c
l3acucm <l3acucm@gmail.com>
2026-03-12 16:04:25 +0600

    sms text fix

--- moses/constants/strings.py
+++ moses/constants/strings.py
@@ -2,6 +2,6 @@ from django.utils.translation import gettext as _
 
 EMAIL_CONFIRMATION_PIN_TITLE = _("Email confirmation PIN")
 EMAIL_CONFIRMATION_PIN_BODY = _("Your email confirmation PIN is %s")
-PHONE_NUMBER_CONFIRMATION_PIN_BODY = _("Your email confirmation PIN is %s")
-PASSWORD_RESET_PIN_BODY = _("Your email confirmation PIN is %s")
+PHONE_NUMBER_CONFIRMATION_PIN_BODY = _("Your phone number confirmation PIN is %s")
+PASSWORD_RESET_PIN_BODY = _("Your password reset PIN is %s")
 PASSWORD_RESET_PIN_TITLE = _("Password reset")
